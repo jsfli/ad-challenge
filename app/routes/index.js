3edf7f56b378f74d66d26b71a8763edc2693c6f8
NadimRai <rai_nadim@yahoo.com>
2017-03-13 15:58:46 -0700

    modified index hbs to display challenges list

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.findAll('challenge');
-  },
-});
+ model() {
+  return Ember.RSVP.hash({
+    challenges: this.store.findAll('challenge'),
+    videos: this.store.findAll('video')
+  });
+}
+})
