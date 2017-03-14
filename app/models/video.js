c6a2009536b2138a58f915379510c3a09383e569
NadimRai <rai_nadim@yahoo.com>
2017-03-13 18:11:00 -0700

    established association between two models

--- app/models/video.js
+++ app/models/video.js
@@ -5,5 +5,6 @@ export default DS.Model.extend({
 	videodescription: DS.attr(),
   videourl: DS.attr(),
   rating: DS.attr(),
-  comment: DS.attr()
+  comment: DS.attr(),
+  challenge: DS.belongsTo('challenge', { async: true }),
 });
