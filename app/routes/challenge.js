caedff0af972ce19a17482ba3fe9813f46120ee9
dxsfung <davxsfung@gmail.com>
2017-03-14 14:35:38 +0800

    resolved conflicts from pull

--- app/routes/challenge.js
+++ app/routes/challenge.js
@@ -4,18 +4,13 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('challenge', params.challenge_id);
   },
-<<<<<<< HEAD
-  
-=======
-});
-  // var a = Ember.$('#output').value;
->>>>>>> c2b1182e7eee00cb59fc44ff02b6f064a9299c94
+
   actions: {
 
-  		
+
 
 	  saveVideo(params) {
-	  		
+
 	      var newVideo = this.store.createRecord('video', params);
 	      var challenge = params.challenge;
 	      challenge.get('videos').addObject(newVideo);
@@ -27,9 +22,8 @@ export default Ember.Route.extend({
 	    delete(video) {
 	    	if (confirm('Are you sure you want to delete this challenge?')){
 	    		video.destroyRecord();
-      			this.transitionTo('/challenge/:challenge_id');	
-	    	}  
+      			this.transitionTo('/challenge/:challenge_id');
+	    	}
     	}
 	}
 });
-
