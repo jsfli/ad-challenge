b469ab27423df28317c8b62c2b4842a9cf2f10bf
NadimRai <rai_nadim@yahoo.com>
2017-03-14 14:54:30 -0700

    created individual route for video

--- app/router.js
+++ app/router.js
@@ -8,7 +8,8 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('challenge', {path: '/challenge/:challenge_id'});
-  
+  this.route('video',{path: '/video/:video_id'});
+
 });
 
 export default Router;
