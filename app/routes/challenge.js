a6a312974a22c250e34435c46dfcb80226618ab8
NadimRai <rai_nadim@yahoo.com>
2017-03-14 14:43:30 -0700

    corrected error in challenge js

--- app/routes/challenge.js
+++ app/routes/challenge.js
@@ -4,16 +4,9 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('challenge', params.challenge_id);
   },
-<<<<<<< HEAD
-  
-=======
-});
-  // var a = Ember.$('#output').value;
->>>>>>> c2b1182e7eee00cb59fc44ff02b6f064a9299c94
+
+ 
   actions: {
-
-  		
-
 	  saveVideo(params) {
 	  		
 	      var newVideo = this.store.createRecord('video', params);
@@ -24,12 +17,14 @@ export default Ember.Route.extend({
 	      });
 	      this.transitionTo('challenge', challenge);
 	    },
+
 	    delete(video) {
 	    	if (confirm('Are you sure you want to delete this challenge?')){
 	    		video.destroyRecord();
       			this.transitionTo('/challenge/:challenge_id');	
 	    	}  
     	}
+	
 	}
 });
 
