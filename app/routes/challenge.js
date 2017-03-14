ac6fdf868951f20f28e44f2970b0cf000e0c55cb
Jeffrey Li <jeffrey.li@cantab.net>
2017-03-14 14:47:46 +0800

    corrected error of challenge js file

--- app/routes/challenge.js
+++ app/routes/challenge.js
@@ -4,22 +4,11 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('challenge', params.challenge_id);
   },
-<<<<<<< HEAD
-  
-=======
-});
-<<<<<<< HEAD
-  
-  actions: {
-=======
-  // var a = Ember.$('#output').value;
->>>>>>> c2b1182e7eee00cb59fc44ff02b6f064a9299c94
-  actions: {
 
-  		
 
->>>>>>> 937deecc498ca5c471c07e9b3cd3e59f1f773f11
+  actions: {
 	  saveVideo(params) {
+
 	      var newVideo = this.store.createRecord('video', params);
 	      var challenge = params.challenge;
 	      challenge.get('videos').addObject(newVideo);
@@ -35,5 +24,6 @@ export default Ember.Route.extend({
       			this.transitionTo('/challenge/:challenge_id');
 	    	}
     	}
+
 	}
 });
