eda0b98da0ebadc4887e2edbdea9ba06d81b3ef3
Jeffrey Li <jeffrey.li@cantab.net>
2017-03-15 15:21:05 +0800

    added sort function to the ratings

--- app/components/review-display.js
+++ app/components/review-display.js
@@ -2,9 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   scores: Ember.computed.mapBy('model.reviews','rating'),
+
   sumOfScores: Ember.computed.sum('scores'),
+
   averageScore: Ember.computed('sumOfScores','scores.length',function(){
     var average = this.get('sumOfScores')/this.get('scores.length');
     return average.toFixed(1);
   }),
+
+  sortBy: ['rating:desc'],
+  sortedReviews: Ember.computed.sort('model.reviews','sortBy'),
 });
