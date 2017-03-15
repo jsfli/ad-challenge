9fb1e2d3b8a157d636ed07ebc054adb5c6153960
Isaac Wong <isaacwongbusiness@gmail.com>
2017-03-15 22:21:38 +0800

    fixing protected routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // beforeModel: function(){
-  //    console.log(this.get('session'));
-  //    if(!this.get('session.isAuthenticated')){
-  //      this.transitionTo('application');
-  //    }
-  //  },
+  beforeModel: function(){
+     console.log(this.get('session'));
+     if(!this.get('session.isAuthenticated')){
+       this.transitionTo('application');
+     }
+   },
 
  model() {
   return Ember.RSVP.hash({
